f79dc93e1b5e6fec47c5ae4eef53bd19c19a5720
Rafa Dominguez <rafa.realmadrid.rdm@gmail.com>
2021-11-17 09:52:43 +0100

    Primer commit

---
+++ src/main/java/comsactuator/micrometer/MicrometerActuatorApplication.java
@@ -0,0 +1,13 @@
+package comsactuator.micrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrometerActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrometerActuatorApplication.class, args);
+	}
+
+}
